efe95d9d565b89c6d333c741182e84252f6533f4
Adithya Renikuntla <adithya.r3639@gmail.com>
2025-09-14 10:53:41 +0530

    feat: create a nested html elements

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "h1" }, // props - attributes set to the html element
-  "Hello World from React!"
-); // create react h1 element
-console.log(heading); // heading is an object
+/**
+ * Create the below HTML Nested Structure using React
+ * 
+ * <div id="parent">
+        <div id="child">
+            <h1>I am H1 Tag</h1>
+            <h2>I am H2 Tag</h2>
+        </div>
+    </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    // create an array for multiple child react elements
+    React.createElement("h1", {}, "I am H1 Tag"),
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ])
+);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root")); // create reactdom root element
-console.log(root);
-root.render(heading); // render converts heading object to the h1 html tag injects to the root div element
+
+root.render(parent); // render converts react element object(s) to the html tag(s) injects to the root div element
 console.log(root);
